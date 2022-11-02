4ff006a474c4186fe5627a435af622df60d61dfe
Abhishek Singh <abi.k.singh64@gmail.com>
2022-11-02 15:38:13 +0100

    register user controller done

---
+++ src/controllers/userController.ts
@@ -0,0 +1,35 @@
+import User from "../models/userModel";
+import { Request, Response } from "express";
+import generateToken from "../utils/generateToken";
+
+//Register a new User
+const registerUser = async (req: any, res: any) => {
+  const { name, email, password } = req.body;
+
+  const userExist = await User.findOne({ email });
+
+  if (userExist) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
+};
+
+export { registerUser };
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,17 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db";
+import userRoutes from "./routes/userRoutes";
 
 dotenv.config();
 
 connectDB();
 
 const app = express();
+
+app.use(express.json());
+
+app.use("/api/users", userRoutes);
 
 const PORT = process.env.PORT || 3001;
 
